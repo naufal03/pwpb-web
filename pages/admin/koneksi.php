6c3cf4d7d98d3f88f7954d0cde2b005f6a518858
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2021-11-06 10:28:55 +0700

    test

--- pages/admin/koneksi.php
+++ pages/admin/koneksi.php
@@ -3,7 +3,7 @@
 	define("db_host", "localhost");
 	define("db_user", "root");
 	define("db_pass", "");
-	define("db_name", "belajar_template");
+	define("db_name", "pwpb_db");
 	
 	$konek = mysqli_connect(db_host, db_user, db_pass, db_name);
 	if (!$konek){
