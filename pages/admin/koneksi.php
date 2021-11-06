24d69c2dee533132e047b274f3996fe6c5b7d315
naufal03 <naufalarkan134@gmail.com>
2021-11-06 10:27:21 +0700

    ganti php

--- pages/admin/koneksi.php
+++ pages/admin/koneksi.php
@@ -3,7 +3,7 @@
 	define("db_host", "localhost");
 	define("db_user", "root");
 	define("db_pass", "");
-	define("db_name", "belajar_template");
+	define("db_name", "db_pwpb");
 	
 	$konek = mysqli_connect(db_host, db_user, db_pass, db_name);
 	if (!$konek){
